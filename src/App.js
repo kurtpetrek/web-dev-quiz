dbdd780750214406ec12248ff04ed86b70b040c9
Kurt Petrek <kurtpetrek@gmail.com>
2017-09-21 01:11:21 -0400

    laysout basic homescreen component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
   render() {
     return (
-      <HomeScreen />
+      <HomeScreen numberOfQuestions={this.state.data.length}/>
     );
   }
 }
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import {styles} from '../constants.js';
+import Button from './Button';
+import { styles } from '../constants.js';
 
 const Heading = styled.h1`
   text-align: center;
   font-family: ${styles.headerFontFamily};
   font-size: 4rem;
+  color: #444;
 
   @media (max-width: 600px) {
     & {
@@ -14,12 +16,126 @@ const Heading = styled.h1`
   }
 `;
 
-export default function HomeScreen(props) {
-  return (
-    <div>
-      <Heading>
-        &lt;HTML&gt; &amp; &#123;CSS&#125; Quiz
-      </Heading>
-    </div>
-  )
+const QuestionSelectorContainer = styled.div`
+  max-width: 700px;
+  margin: 20px auto;
+  border-radius: 3px;
+  border-left: 2px solid #444;
+  border-bottom: 2px solid #444;
+  padding: 1rem;
+  position: relative;
+  box-shadow: 0px 0px 0px #444,
+  -1px 1px 0px #444,
+  -2px 2px 0px #444,
+  -3px 3px 0px #444,
+  -4px 4px 0px #444,
+  -5px 5px 0px #444,
+  -6px 6px 0px #444,
+  -7px 7px 0px #444,
+  -8px 8px 0px #444,
+  -9px 9px 0px #444,
+  -10px 10px 0px #444;
+
+  @media (max-width: 750px) {
+    box-shadow: none;
+    border: none;
+    &::before {
+      display: none;
+    }
+    &::after {
+      display: none;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background: linear-gradient(to right, #444, rgba(68,68,68,0));
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 2px;
+    right: 0;
+    top: 0;
+    background: linear-gradient(to top, #444, rgba(68,68,68,0));
+  }
+
+  p {
+    font-size: 1.3rem;
+  }
+`;
+
+export default class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      maxQuestions: props.numberOfQuestions,
+      numberOfQuestions: props.numberOfQuestions
+    }
+  }
+
+  collectInput = (e) => {
+    let val = parseInt(e.target.value, 10);
+    this.setState((prevState)=> {
+      prevState.numberOfQuestions = val;
+      return prevState;
+    })
+  }
+
+  onButtonClick = (e) => {
+    console.log(this.state);
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Heading>
+          &lt;HTML&gt; &amp; &#123;CSS&#125; Quiz
+        </Heading>
+
+        <QuestionSelectorContainer>
+          <p>Choose the number of questions: </p>
+          <form>
+            <label>
+              <input
+                type="radio"
+                value="10"
+                checked={this.state.numberOfQuestions === 10}
+                onChange={this.collectInput}
+              />
+              10
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="20"
+                checked={this.state.numberOfQuestions === 20}
+                onChange={this.collectInput}
+              />
+              20
+            </label>
+            <label>
+              <input
+                type="radio"
+                checked={this.state.numberOfQuestions === this.state.maxQuestions}
+                value={this.state.maxQuestions}
+                onChange={this.collectInput}
+                />
+                {this.state.maxQuestions}
+            </label>
+          </form>
+          <Button handleClick={this.onButtonClick}>Start Quiz</Button>
+        </QuestionSelectorContainer>
+      </div>
+    )
+  }
 }
